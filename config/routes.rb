efeca4df87e0053fc3468927c5d0c52fa90b3511
jonas-45 <jonaswullah@gmail.com>
2023-08-30 06:43:39 +0000

    Add route to reservations api data

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
    namespace :api do
     get 'motorcycles', to: 'motorcycles#index'
+    get 'reservations', to: 'reservations#index'
   end 
   # Defines the root path route ("/")
   # root "articles#index"
