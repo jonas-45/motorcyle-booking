3c7b216541627a52034b415b560a5ab21f63414c
jonas-45 <jonaswullah@gmail.com>
2023-09-06 16:49:33 +0000

    Add validation for saving user reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,5 @@ class Reservation < ApplicationRecord
   belongs_to :motorcycle
   belongs_to :user
 
-  validates :reservation_time, presence: true
   validates :reservation_date, presence: true
 end
