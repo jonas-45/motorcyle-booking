276f6cfb6f8f5b57bcfe4a01589bb86a4a5452e5
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-09-07 15:24:45 +0500

    Fix the port number from 3002 to 3001

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3002' # Replace with the origin of your React app
+    origins 'http://localhost:3001' # Replace with the origin of your React app
 
     # Allow the necessary HTTP methods and headers
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
